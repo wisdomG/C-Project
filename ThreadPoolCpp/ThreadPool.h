0e8ddb799dbc143d7b9ee4612a652d36da90ed89
WisdomCao <kenpluver@hotmail.com>
2018-04-09 13:23:34 +0800

    add a makefile

--- ThreadPoolCpp/ThreadPool.h
+++ ThreadPoolCpp/ThreadPool.h
@@ -1,28 +1,68 @@
-#ifdef _THREAD_POOL_H
-#define _THREAD_POOL_H
+#ifndef _THREAD_POOL_H_
+#define _THREAD_POOL_H_
 
 #include <iostream>
 #include <string>
 #include <future>
 #include <thread>
-#include <funtion>
+#include <queue>
 #include <mutex>
 #include <condition_variable>
 using namespace std;
 
 class ThreadPool {
     public:
-        ThreadPool(size_t threads);
+        ThreadPool(size_t threads): stop(false) {
+            for (int i = 0; i < threads; ++i) {
+                workers.emplace_back([this]{
+                        for (;;) {
+                            function<void()> task;
+                            {
+                                unique_lock<mutex> lock(this->queue_mutex);
+                                this->condition.wait(lock, [this]{return this->stop || !this->tasks.empty();});
+                                if (this->stop && this->tasks.empty()) return;
+                                task = move(this->tasks.front());
+                                this->tasks.pop();
+                            }
+                            task();
+                        }
+                        });
+            }
+        }
+
         template<typename F, typename... Args>
         auto enqueue(F&& f, Args&&... args)
-        -> future<typename result_of<F(Arg...)>::type;
-        ~ThreadPool();
+        -> future<typename result_of<F(Args...)>::type>
+        {
+            using return_type = typename result_of<F(Args...)>::type;
+            auto task = make_shared<packaged_task<return_type()>>(bind(forward<F>(f), forward<Args>(args)...));
+            future<return_type> res = task->get_future();
+            {
+                unique_lock<mutex> lock(queue_mutex);
+                if (stop) throw runtime_error("enqueue on stopped ThreadPool");
+                tasks.emplace([task]{(*task)();});
+            }
+            condition.notify_one();
+            return res;
+        }
+
+        ~ThreadPool() {
+            {
+                unique_lock<mutex> lock(queue_mutex);
+                stop = true;
+            }
+            condition.notify_all();
+            for (thread& worker: workers) {
+                worker.join();
+            }
+        }
+
     private:
         vector<thread> workers;
         queue<function<void()>> tasks;
         mutex queue_mutex;
         condition_variable condition;
         bool stop;
-}
+};
 
 #endif
--- ThreadPoolCpp/main.cpp
+++ ThreadPoolCpp/main.cpp
@@ -8,7 +8,7 @@ int main() {
     for (int i = 0; i < 8; ++i) {
         results.emplace_back(pool.enqueue([i]{
                     cout << "hello " << i << endl;
-                    this_thread::sleep_for(chrono::second(1));
+                    this_thread::sleep_for(chrono::seconds(1));
                     cout << "world" << i << endl;
                     return "---thread " + to_string(i) + "finished.---";
                     }));
