1e5587ca2ae5bb3e90b5b2d81a81a57d57eb5986
wise <wisdom610@163.com>
2018-08-18 01:21:13 -0700

    add some comments

--- ChatRoom/server.c
+++ ChatRoom/server.c
@@ -11,15 +11,15 @@
 #include <stdlib.h>
 #include <poll.h>
 
-#define USER_LIMIT 5
+#define USER_LIMIT 2
 #define BUFFER_SIZE 64
 #define FD_LIMIT 65535  /* 文件描述符数量限制 */
 
 /* 客户端信息 */
 struct client_data {
     sockaddr_in address;
-    char *write_buf;
-    char buf[BUFFER_SIZE];
+    char *write_buf;        // 写出数据
+    char buf[BUFFER_SIZE];  // 数据读到buf中
 };
 
 int setnonblocking(int fd) {
@@ -41,6 +41,7 @@ int main(int argc, char* argv[]) {
     struct sockaddr_in address;
     bzero(&address, sizeof(address));
     address.sin_family = AF_INET;
+    /* inet_pton将字符串形式的IP地址转换成uint32形式的 */
     inet_pton(AF_INET, ip, &address.sin_addr);
     address.sin_port = htons(port);
 
@@ -57,11 +58,13 @@ int main(int argc, char* argv[]) {
     /* 共计USER_LIMIT+1个文件描述符，+1是因为还有一个listenfd */
     pollfd fds[USER_LIMIT + 1];
     int user_counter = 0;
+
     for (int i = 1; i <= USER_LIMIT; ++i) {
         fds[i].fd = -1;   // 由后续的connfd来填充
         fds[i].events = 0;
     }
     fds[0].fd = listenfd;
+    /* listenfd上的两个事件，可读事件和错误事件 */
     fds[0].events = POLLIN | POLLERR;
     fds[0].revents = 0;
 
@@ -71,8 +74,11 @@ int main(int argc, char* argv[]) {
             printf("poll failed\n");
             break;
         }
+        /* 遍历这几个文件描述符 */
         for (int i = 0; i < user_counter + 1; ++i) {
+            /* listenfd上的读事件，说明有新的连接 */
             if ((fds[i].fd == listenfd) && (fds[i].revents & POLLIN)) {
+                /* sockaddr_in是IPv4的专用地址结构 */
                 struct sockaddr_in client_address;
                 socklen_t client_addrlength = sizeof(client_address);
                 int connfd = accept(listenfd, (struct sockaddr*)(&client_address), &client_addrlength);
@@ -83,6 +89,7 @@ int main(int argc, char* argv[]) {
                 if (user_counter >= USER_LIMIT) {
                     const char *info = "too many users\n";
                     printf("%s\n", info);
+                    /* 发送提示信息，然后关闭企图连接的文件描述符 */
                     send(connfd, info, strlen(info), 0);
                     close(connfd);
                     continue;
@@ -98,8 +105,10 @@ int main(int argc, char* argv[]) {
             else if(fds[i].revents & POLLERR) {
                 printf("get an error from %d\n", fds[i].fd);
                 char errors[100];
-                memset(errors, '\0', 100);
                 socklen_t len = sizeof(errors);
+                memset(errors, '\0', len);
+                /* getsockopt获取socket文件描述符的信息 */
+                /* 获取并清除socket文件描述符上的错误状态，即清除本次错误，下次可以继续执行 */
                 if (getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &errors, &len) < 0) {
                     printf("get socket option failed\n");
                 }
@@ -107,21 +116,25 @@ int main(int argc, char* argv[]) {
             }
             else if (fds[i].revents & POLLRDHUP) {
                 /* 客户机关闭连接 */
-                /* 把最后一个fd拿过来填充到要被关闭的fd位置，然后再将counter-- */
+                /* 把最后一个fd拿过来填充到要被关闭的fd位置，然后再将counter--，i--，
+                 * 以便下次遍历还能遍历到到这个文件描述符 */
                 users[fds[i].fd] = users[fds[user_counter].fd];
                 close(fds[i].fd);
                 fds[i] = fds[user_counter];
+                /* i--,cnt++是为了其 */
                 i--;
                 user_counter--;
                 printf("a client left\n");
             }
             else if (fds[i].revents & POLLIN) {
+                /* 文件描述符上有数据可读 */
                 int connfd = fds[i].fd;
                 memset(users[connfd].buf, '\0', BUFFER_SIZE);
                 ret = recv(connfd, users[connfd].buf, BUFFER_SIZE - 1, 0);
                 printf("get %d bytes of client data %s from %d\n", ret, users[connfd].buf, connfd);
-                if (ret < 0) {
-                    if (errno != EAGAIN) {
+                if (ret < 0) { // 数据接收出错
+                /* EAGAIN说明数据太多报错，实际不影响程序，由于是LT，下次正常接收即可 */
+                    if (errno != EAGAIN) { // != EAGAIN说明镇的出错
                         close(connfd);
                         users[fds[i].fd] = users[fds[user_counter].fd];
                         fds[i] = fds[user_counter];
@@ -129,17 +142,18 @@ int main(int argc, char* argv[]) {
                         user_counter--;
                     }
                 }
-                else if (ret == 0) {
+                else if (ret == 0) { // 说明是客户端调用close主动关闭了连接
 
                 }
                 else {
-                    /* 通知其他socket准备写数据 */
+                    /* 通知其他socket准备写数据，即将某个socket发过来的数据
+                     * 都发送给其余几个socket */
                     for (int j = 1; j <= user_counter; ++j) {
                         if (fds[j].fd == connfd) {
                             continue;
                         }
-                        fds[j].events |= ~POLLIN;
-                        fds[j].events |= POLLOUT;
+                        fds[j].events |= ~POLLIN; // 关闭读事件
+                        fds[j].events |= POLLOUT; // 打开写事件
                         users[fds[j].fd].write_buf = users[connfd].buf;
                     }
                 }
@@ -154,7 +168,6 @@ int main(int argc, char* argv[]) {
                 fds[i].events |= ~POLLOUT;
                 fds[i].events |= POLLIN;
             }
-
         }
     }
 
